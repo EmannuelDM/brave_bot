f74373c48a37501d49ae2bbcedbfa1dc31104965
unknown <ediazmaccio@clicoh.com>
2022-02-26 20:26:43 -0300

    mouse movement and new random times

--- main.py
+++ main.py
@@ -13,11 +13,15 @@ def make_money():
     while True:
         # Open 10 tabs
         for i in range(10):
-            # Wait between 3 and 6 seconds for each tab to open
-            time_delay = random.randrange(3, 6)
+            # Wait between 8 and 15 seconds for each tab to open
+            time_delay = random.randrange(8, 15)
             time.sleep(time_delay)
             with pyautogui.hold('ctrl'):
                 pyautogui.press(['t'])
+            # Moves mouse to coordinates X and Y , with time_delay
+            time_delay = random.randrange(2, 3)
+            time.sleep(time_delay)
+            pyautogui.moveTo(time_delay * 100, time_delay*150, time_delay)
         # Close 10 tabs
         for i in range(10):
             # Wait between 3 and 6 seconds for each tab to close
@@ -25,6 +29,10 @@ def make_money():
             time.sleep(time_delay)
             with pyautogui.hold('ctrl'):
                 pyautogui.press(['w'])
+            # Moves mouse to coordinates X and Y , with time_delay
+            time_delay = random.randrange(2, 3)
+            time.sleep(time_delay)
+            pyautogui.moveTo(time_delay * 100, time_delay * 150, time_delay)
 
 
 # Press the green button in the gutter to run the script.
